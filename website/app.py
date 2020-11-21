a9ddc2e4edb5af0d8d2f59b389a99962c4249c4b
Vinh Huynh <vhuynh36@gatech.edu>
2020-11-21 02:05:24 -0500

    added static dir for uploaded file

--- website/app.py
+++ website/app.py
@@ -21,17 +21,25 @@
 # 	app.run()
 
 from flask import Flask, render_template, request
+import os
+import shutil
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'upload')
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def upload_file():
 	return render_template('index.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_files():
-	print("Tako")
+	if request.method == 'POST':
+		f = request.files['file']
+		if os.path.exists(app.config['UPLOAD_FOLDER']) and os.path.isdir(app.config['UPLOAD_FOLDER']):
+			shutil.rmtree(app.config['UPLOAD_FOLDER'])
+		os.mkdir(app.config['UPLOAD_FOLDER'])
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'], "input.mp4"))
 	return render_template('result.html')
 
-		
 if __name__ == '__main__':
    app.run(debug = True)
