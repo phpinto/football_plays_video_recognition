36144748967557df2f71d4f8f1097e7c642b094f
Vinh Huynh <vhuynh36@gatech.edu>
2020-12-03 18:50:30 -0500

    clean up website code

--- website/app.py
+++ website/app.py
@@ -10,6 +10,7 @@ from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import sequence
 from keras.models import Model
 import time
+# import matplotlib.pyplot as plt
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'upload')
@@ -114,15 +115,12 @@ def get_prediction(images):
 
 @app.after_request
 def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    # r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+
+	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	r.headers["Pragma"] = "no-cache"
+	r.headers["Expires"] = "0"
+	# r.headers['Cache-Control'] = 'public, max-age=0'
+	return r
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug=False)
